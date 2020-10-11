2d2d0334666c57fbd42e291d4bccb609b1b303c3
FujimuraKaito <>
2020-10-11 14:42:20 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // ReactはJSXを使うときは必要(for JSX)
 import React from 'react';
 
+// functional component
 function App() {
   return (
     <React.Fragment>
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         {/* classNameはhtmlのclassと一緒 */}
         <h1>Hello, React!</h1>
+        <Cat />
         <label htmlFor="bar">bar</label>
         <input type="text" onClick={() => {console.log('this is clicked.')}}></input>
         <input type="text" onChange={() => {console.log('this is changed.')}}></input>
@@ -17,7 +19,13 @@ function App() {
   );
 }
 
-// 以下のようにトランスパイルされる
+// こんな漢字の関数型定義もできる
+const Cat = () => {
+  return <div><h1>Hello, cat!</h1></div>
+}
+
+
+// 以下のようにトランスパイルされる(babel)
 // function App() {
 //   return (
 //     React.createElement(
